a55b34a8a0921e48d2fbaa83fc6c0911006041a4
diego carvalho <diegohsc@gmail.com>
2022-02-22 22:48:57 -0300

    firt commit

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,29 @@
+
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from "App/Models/User";
+
+export default class AuthController {
+  public async register({request,response}:HttpContextContract){
+  try {
+      const data = request.only(["email" , "password"]);
+      const user = await User.create(data);
+      return user;
+  } catch (error) {
+      response.status(500).send( "erro ao validar usuário");
+  }
+      
+  }
+  
+
+  public async authenticate({response,request,auth}:HttpContextContract){
+      try {
+          const {email, password} = request.all();
+          const {token} = await auth.use('api').attempt(email,password);
+          const user = await User.findByOrFail("email", email);
+          return{token,user};
+      } catch (error) {
+          response.status(500).send("erro ao cadastrar o usuario")
+      }
+
+  }
+}
